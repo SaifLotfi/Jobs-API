69eeffab10d4ae1a34e0d74a2eb51b32f2d2031c
Saif <swaifweaa@gmail.com>
2023-09-03 15:26:20 +0300

    added getAllJobs - getJob logic

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,21 +1,31 @@
-import { StatusCodes } from "http-status-codes";
-import Job from "../models/Job.js";
+import { StatusCodes } from 'http-status-codes';
+import Job from '../models/Job.js';
+import {NotFoundError} from '../errors/index.js';
 
 const getAllJobs = async (req, res) => {
-    res.send('get all jobs');
+    const jobs = await Job.find({ createdBy: req.user.userId }).sort(
+        'createdAt'
+    );
+    res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
 
 const getJob = async (req, res) => {
-    res.send('get job');
+    const {user:{userId},params:{id:jobId}} = req;
+    const job = await Job.findOne({_id:jobId,createdBy:userId});
+    if(!job){
+        throw new NotFoundError(`no such job with id : ${jobId}`);
+    }
+    res.status(StatusCodes.OK).json({ job});
+
 };
 
 const createJob = async (req, res) => {
     req.body.createdBy = req.user.userId;
     const job = new Job(req.body);
-    
+
     await job.save();
-    
-    res.status(StatusCodes.CREATED).json({job});
+
+    res.status(StatusCodes.CREATED).json({ job });
 };
 
 const updateJob = async (req, res) => {
--- middleware/authentication.js
+++ middleware/authentication.js
@@ -3,10 +3,12 @@ import { UnauthenticatedError } from '../errors/index.js';
 import User from '../models/User.js';
 
 const isAuth = (req, res, next) => {
+
     const authToken = req.get('Authorization').split(' ')[1];
     if (!authToken) {
         throw new UnauthenticatedError('No Token Provided!');
     }
+
     try {
         const decodedToken = jwt.verify(authToken, process.env.JWT_SECRET);
         req.user = {userId:decodedToken.userId,name:decodedToken.name};
