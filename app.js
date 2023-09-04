e56732ac1960b4a0e6c0ddc96d832efed7430b4e
Saif <swaifweaa@gmail.com>
2023-09-04 10:08:46 +0300

    added security packages

--- app.js
+++ app.js
@@ -9,13 +9,26 @@ import connectDB from './db/connect.js';
 import authRouter from './routes/auth.js';
 import jobsRouter from './routes/jobs.js';
 
+//security packages :
+import helmet from 'helmet';
+import cors from 'cors';
+import xss from 'xss-clean';
+import rateLimiter from 'express-rate-limit';
 
 // error handler
 import notFoundMiddleware from'./middleware/not-found.js';
 import errorHandlerMiddleware from'./middleware/error-handler.js';
 import isAuth from './middleware/authentication.js';
 
+app.use(rateLimiter({
+    windowMs:15*60*1000,
+    max:100
+}));
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xxs());
+
 // extra packages
 
 // routes
