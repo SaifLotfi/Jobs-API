aca7d4b95d58b0e74b5acf65abca6273fdf5e4ab
Saif <swaifweaa@gmail.com>
2023-09-04 09:16:33 +0300

    added delete and update job controller

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,6 +1,6 @@
 import { StatusCodes } from 'http-status-codes';
 import Job from '../models/Job.js';
-import {NotFoundError} from '../errors/index.js';
+import { BadRequestError, NotFoundError } from '../errors/index.js';
 
 const getAllJobs = async (req, res) => {
     const jobs = await Job.find({ createdBy: req.user.userId }).sort(
@@ -10,13 +10,15 @@ const getAllJobs = async (req, res) => {
 };
 
 const getJob = async (req, res) => {
-    const {user:{userId},params:{id:jobId}} = req;
-    const job = await Job.findOne({_id:jobId,createdBy:userId});
-    if(!job){
+    const {
+        user: { userId },
+        params: { id: jobId },
+    } = req;
+    const job = await Job.findOne({ _id: jobId, createdBy: userId });
+    if (!job) {
         throw new NotFoundError(`no such job with id : ${jobId}`);
     }
-    res.status(StatusCodes.OK).json({ job});
-
+    res.status(StatusCodes.OK).json({ job });
 };
 
 const createJob = async (req, res) => {
@@ -29,11 +31,44 @@ const createJob = async (req, res) => {
 };
 
 const updateJob = async (req, res) => {
-    res.send('update job');
+    const {
+        body: { company, position },
+        user: { userId },
+        params: { id: jobId },
+    } = req;
+
+    if (company === '' || position === '') {
+        throw new BadRequestError(
+            'Company Or Position Fields cannot be Empty!'
+        );
+    }
+
+    const job = await Job.findOneAndUpdate(
+        { _id: jobId, createdBy: userId },
+        req.body,
+        { new: true, runValidators: true }
+    );
+
+    if (!job) {
+        throw new NotFoundError(`no such job with id : ${jobId}`);
+    }
+
+    res.status(StatusCodes.OK).json({ job });
 };
 
 const deleteJob = async (req, res) => {
-    res.send('delete job');
+    const {
+        user: { userId },
+        params: { id: jobId },
+    } = req;
+
+    const job = await Job.findOneAndRemove({ _id: jobId, createdBy: userId });
+
+    if (!job) {
+        throw new NotFoundError(`no such job with id : ${jobId}`);
+    }
+
+    res.status(StatusCodes.OK).send();
 };
 
 export { getAllJobs, getJob, createJob, updateJob, deleteJob };
