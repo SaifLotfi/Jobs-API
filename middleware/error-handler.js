ca5a289b2c660752c7ee662f9cf7134c1b41f859
Saif <swaifweaa@gmail.com>
2023-09-04 09:35:38 +0300

    improved error handling

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,33 @@
-import  CustomAPIError  from '../errors/custom-api.js';
+// import CustomAPIError from '../errors/custom-api.js';
 import { StatusCodes } from 'http-status-codes';
+
 const errorHandlerMiddleware = (err, req, res, next) => {
-    if (err instanceof CustomAPIError) {
-        return res.status(err.statusCode).json({ msg: err.message });
+    let customError = {
+        statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+        msg: err.message || 'Some Thing Went Wrong , Try Again Later!',
+    };
+    // if (err instanceof CustomAPIError) {
+    //     return res.status(err.statusCode).json({ msg: err.message });
+    // }
+    if (err.name === 'ValidationError') {
+        customError.msg = Object.values(err.errors)
+            .map((item) => item.message)
+            .join(',');
+        customError.statusCode = 400;
     }
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+    if (err.name === 'CastError') {
+        customError.msg = `no item found with the id : ${err.value}`;
+        customError.statusCode = 404;
+    }
+    if (err.code && err.code === 11000) {
+        customError.msg = `Duplicate value entered for ${Object.keys(
+            err.keyValue
+        )} field , Please Choose Another Value!`;
+        customError.statusCode = 400;
+    }
+    // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+
+    return res.status(customError.statusCode).json({ msg: customError.msg });
 };
 
 export default errorHandlerMiddleware;
