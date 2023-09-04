0e2a1a3808d4dfadbad9a5e641081570ac616190
Saif <swaifweaa@gmail.com>
2023-09-04 12:06:31 +0300

    Added Swagger Docs

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import 'dotenv/config.js';
-import 'express-async-errors'
+import 'express-async-errors';
 import express from 'express';
 const app = express();
 
@@ -15,15 +15,21 @@ import cors from 'cors';
 import xss from 'xss-clean';
 import rateLimiter from 'express-rate-limit';
 
+//Swagger
+import swaggerUI from 'swagger-ui-express';
+import YAML from 'yamljs';
+const swaggerDocument = YAML.load('./swagger.yaml');
 // error handler
-import notFoundMiddleware from'./middleware/not-found.js';
-import errorHandlerMiddleware from'./middleware/error-handler.js';
+import notFoundMiddleware from './middleware/not-found.js';
+import errorHandlerMiddleware from './middleware/error-handler.js';
 import isAuth from './middleware/authentication.js';
 
-app.use(rateLimiter({
-    windowMs:15*60*1000,
-    max:100
-}));
+app.use(
+    rateLimiter({
+        windowMs: 15 * 60 * 1000,
+        max: 100,
+    })
+);
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
@@ -32,8 +38,14 @@ app.use(xss());
 // extra packages
 
 // routes
-app.use('/api/v1/auth',authRouter);
-app.use('/api/v1/jobs',isAuth,jobsRouter);
+app.use('/', (req, res, next) => {
+    res.send('<h1>Jobs API</h1> <a href="/api-docs">Documentation</a>');
+});
+
+app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDocument));
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', isAuth, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
