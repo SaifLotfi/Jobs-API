7f122c77ea58f3268328660159ed98e4b1ce762f
Saif <swaifweaa@gmail.com>
2023-09-22 10:22:52 +0300

    improved isAuth function

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,20 +1,21 @@
 import jwt from 'jsonwebtoken';
 import { UnauthenticatedError } from '../errors/index.js';
-import User from '../models/User.js';
 
 const isAuth = (req, res, next) => {
-
-    const authToken = req.get('Authorization').split(' ')[1];
+    let authToken;
+    const authHeader = req.get('Authorization');
+    if (authHeader) {
+        authToken = authHeader.split(' ')[1];
+    }
     if (!authToken) {
         throw new UnauthenticatedError('No Token Provided!');
     }
 
     try {
         const decodedToken = jwt.verify(authToken, process.env.JWT_SECRET);
-        req.user = {userId:decodedToken.userId,name:decodedToken.name};
+        req.user = { userId: decodedToken.userId, name: decodedToken.name };
         next();
     } catch (err) {
-        
         throw new UnauthenticatedError('Authentication Invalid!');
     }
 };
