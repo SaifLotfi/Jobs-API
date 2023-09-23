972a0d7ac168383d9356011c9de088d5c16fd585
Saif <swaifweaa@gmail.com>
2023-09-23 12:22:04 +0300

    added reset password setup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
 import { StatusCodes } from 'http-status-codes';
 import User from '../models/User.js';
-import {BadRequestError,UnauthenticatedError} from '../errors/index.js';
+import {BadRequestError,NotFoundError,UnauthenticatedError} from '../errors/index.js';
 import  bcrypt  from 'bcryptjs';
+import Token from '../models/Token'
 
 const register = async (req, res) => {
     const user = await User.create({ ...req.body });
@@ -25,5 +26,27 @@ const login = async (req, res) => {
     
     res.status(StatusCodes.OK).json({ user: { name: user.name }, token:user.createJwt() });
 };
+
+const requestResetPassword = async (req, res) => {
+    const {email} = req.body;
+    if(!email){
+        throw new BadRequestError('Please Provide email!');
+    }
+    const user = await User.findOne({email});
+    if(!user){
+        throw new NotFoundError('User not found!');
+    }
+    let token = Token.findOne({userId:user._id});
+    if(token){
+        await token.deleteOne();
+    }
+    token = crypto.randomBytes(32).toString('hex');
+    const resetPassowrdToken = new Token({
+        userId :user._id,
+        token
+    });
+    await resetPassowrdToken.save();
+}
+
 
 export { register, login };
